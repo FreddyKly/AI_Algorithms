b0c3e008e9cafa46e1a7fe3951941d4406864a0b
FreddyKly <Frederik.Kliemt@web.de>
2023-01-15 16:45:20 +0100

    update nine mens morris

--- MCTS_Nine_Mens_Morris.py
+++ MCTS_Nine_Mens_Morris.py
@@ -1,4 +1,4 @@
-
+import random
 
 white_pieces = 9
 black_pieces = 9
@@ -6,7 +6,8 @@ black_pieces = 9
 possible_positions = [
     11, 12, 13, 14, 15, 16, 17, 18,
     21, 22, 23, 24, 25, 26, 27, 28,
-    31, 32, 33, 34, 35, 36, 37, 38]
+    31, 32, 33, 34, 35, 36, 37, 38
+    ]
 
 mill_positions = [
     (11, 12, 13), (13, 14, 15), (15, 16, 17), (17, 18, 11),
@@ -15,8 +16,23 @@ mill_positions = [
     (12, 22, 32), (14, 24, 34), (16, 26, 36), (18, 28, 38)
     ]
 
+occupied_positions = []
+
+def turn():
+    next_turn_pos = random.randint(0, len(possible_positions))
+    possible_positions.pop(next_turn_pos)
+
 def MCTS():
     pass 
 
 if __name__ == "__main__":
-    MCTS()
+    while True:
+        print("Available positions: ", possible_positions)
+        user_turn_pos = int(input("Enter Position where you want to place a piece on: "))
+        if user_turn_pos in possible_positions:
+            pass
+        else:
+            print("The position you selected was not available. Try again: ")
+        turn()
+        MCTS()
+    
