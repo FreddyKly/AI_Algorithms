2b7d0e5267afe2f3eb67c394b42a3b7d7b407dee
FreddyKly <Frederik.Kliemt@web.de>
2023-01-16 23:48:23 +0100

    new class structure

--- MCTS_Nine_Mens_Morris.py
+++ MCTS_Nine_Mens_Morris.py
@@ -4,8 +4,7 @@ import time
 # Representation of the board
 class Board():
 
-    def __init__(self) -> None:
-        
+    def __init__(self) -> None:    
         self.num_white_pieces = 9 # pieces that have not been played 
         self.num_black_pieces = 9
 
@@ -76,39 +75,48 @@ class Node():
         possible_actions = board.possible_positions
         action = possible_actions[random.randint(0, len(possible_actions))] # Select a random position from the available ones
 
+class Player():
+    def __init__(self, is_AI, board) -> None:
+        self.is_AI = is_AI # True for AI, False for Human
 
-def AI_turn(borad):
-    ai_turn_pos = MCTS() 
-    random.randint(0, len(board.possible_positions))
-    board.possible_positions.pop(ai_turn_pos)
-    board.num_black_pieces = board.num_black_pieces - 1
+    def turn(self, board):
+        if self.is_AI:
+            ai_turn_pos = MCTS() 
+            random.randint(0, len(board.possible_positions))
+            board.possible_positions.pop(ai_turn_pos)
+            board.num_black_pieces = board.num_black_pieces - 1
+        else:
+            self.Human_turn()
+    
 
-def Human_turn(board):
-    user_turn_pos = int(input("Enter Position where you want to place a piece on: "))
-    if user_turn_pos in board.possible_positions:
-        num_white_pieces = num_white_pieces - 1
-        board.possible_positions.pop(user_turn_pos)
-        board.white_positions.append(user_turn_pos)
-        if is_mill(board.white_positions):
-            remove_piece_pos = int(input("You Have a Mill! Enter the position of the piece you want to be removed: "))
+    def Human_turn(self, board):
+        user_turn_pos = int(input("Enter Position where you want to place a piece on: "))
+        if user_turn_pos in board.possible_positions:
+            num_white_pieces = num_white_pieces - 1
+            board.possible_positions.pop(user_turn_pos)
+            board.white_positions.append(user_turn_pos)
+            if self.is_mill(board.white_positions):
+                remove_piece_pos = int(input("You Have a Mill! Enter the position of the piece you want to be removed: "))
 
-            while remove_piece_pos not in board.black_positions: 
-                print("The position you selected is not one with a black piece on it. Try again.") 
-                print("Black pieces on the Board: ", board.black_positions)
-                remove_piece_pos = int(input("Enter the position of the piece you want to be removed: "))
+                while remove_piece_pos not in board.black_positions: 
+                    print("The position you selected is not one with a black piece on it. Try again.") 
+                    print("Black pieces on the Board: ", board.black_positions)
+                    remove_piece_pos = int(input("Enter the position of the piece you want to be removed: "))
 
-            board.black_positions.remove(remove_piece_pos)
-    else:
-        print("The position you selected was not available. Try again: ")
-        Human_turn()
-        return
+                board.black_positions.remove(remove_piece_pos)
+        else:
+            print("The position you selected was not available. Try again: ")
+            self.Human_turn()
+            return
 
-def is_mill(positions):
-    return any(set(tuple).issubset(positions) for tuple in board.mill_positions)
+    def is_mill(positions):
+        return any(set(tuple).issubset(positions) for tuple in board.mill_positions)
 
 
 if __name__ == "__main__":
     board = Board()
+    AI = Player(True)
+    Human = Player(False)
     print("You will be playing Nine Men's Morris as White against a AI powered by MCTS!")
     while True:
         print("Available positions: ", board.possible_positions)
@@ -122,6 +130,6 @@ if __name__ == "__main__":
             else:
                 print("That's a draw.")
             break
-        Human_turn(board)
-        AI_turn(board)
+        AI.turn()
+        Human.turn()
     
