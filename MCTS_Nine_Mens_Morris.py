8154e139ef2e5f307a57ff2412d5a07579c13f09
FreddyKly <Frederik.Klie@gmail.com>
2023-01-16 19:39:25 +0100

    update

--- MCTS_Nine_Mens_Morris.py
+++ MCTS_Nine_Mens_Morris.py
@@ -1,6 +1,7 @@
 import random
 import time
 
+# Representation of the board
 class Board():
 
     def __init__(self) -> None:
@@ -25,10 +26,62 @@ class Board():
 
         self.black_positions = []
 
+# Can perform the Monte Carlo Tree Search
+class MCTS():
+
+    def __init__(self, board) -> None:
+        self.board = board
+
+    def play_round(self):
+        root = Node(None, self.board.possible_positions)
+        while time.time() + 5 > time.time():
+            leaf = self.selectNode(root)
+            child = self.expand(leaf)
+            result = self.simulate(child)
+            self.backpropagate(result)
+
+    def simulate(self, node):
+        board_copy = self.board
+        while True:
+            if node.is_terminal():
+                reward = node.get_score()
+                return reward
+            node = node.find_random_child(board_copy)
+
+    def selectNode():
+        pass
+
+    def expand():
+        pass
+
+    def backpropagate():
+        pass
+
+
+# Representaion of a Node
+class Node():
+    def __init__(self, parent, children) -> None:
+        self.parent = parent
+        self.children = children
+
+    def is_terminal(board):
+        if board.num_white_pieces + board.num_black_pieces == 0:
+            return True
+        return False
+
+    def get_score():
+        pass
+
+    def find_random_child(board):
+        possible_actions = board.possible_positions
+        action = possible_actions[random.randint(0, len(possible_actions))] # Select a random position from the available ones
+
+
 def AI_turn(borad):
     ai_turn_pos = MCTS() 
-    random.randint(0, len(possible_positions))
-    possible_positions.pop(ai_turn_pos)
+    random.randint(0, len(board.possible_positions))
+    board.possible_positions.pop(ai_turn_pos)
+    board.num_black_pieces = board.num_black_pieces - 1
 
 def Human_turn(board):
     user_turn_pos = int(input("Enter Position where you want to place a piece on: "))
@@ -52,42 +105,7 @@ def Human_turn(board):
 
 def is_mill(positions):
     return any(set(tuple).issubset(positions) for tuple in board.mill_positions)
-    
 
-class MCTS():
-
-    def __init__(self, board) -> None:
-        self.board = board
-
-    def play_round(self):
-        root = Node(None, self.board.possible_positions)
-        while time.time() + 5 > time.time():
-            leaf = self.selectNode(root)
-            child = self.expand(leaf)
-            result = self.simulate(child)
-
-    def simulate(self, node):
-        while True:
-            if node.is_terminal():
-                reward = node.get_score()
-                return reward
-            node = node.find_random_child()
-
-
-
-class Node():
-    def __init__(self, parent, children) -> None:
-        self.parent = parent
-        self.children = children
-
-    def is_terminal():
-        pass
-
-    def get_score():
-        pass
-
-    def find_random_child():
-        pass
 
 if __name__ == "__main__":
     board = Board()
